76fac2f3d5f1f0da55979933a7b6c2c0bc82c852
= <=>
2023-11-02 11:35:01 +0530

    files updated

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// document.getElementsByClassName('up3')[0].addEventListener('click',()=>{
+//     var allElements = document.querySelectorAll('*');
+    
+//     allElements.forEach(function(element) {
+//         element.style.backgroundColor = 'lightblue';
+//     });   
+    
+// })
+
+console.log('clicked')
